9e8772d9ac2419c03d216df7730e1b552a118269
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2025-05-27 11:51:03 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const TAOSTATS_API_BASE = 'https://api.taostats.io';
+
+app.use(async (req, res) => {
+
+
+  try {
+    const path = req.originalUrl;
+    console.log(path)
+    const url = `${TAOSTATS_API_BASE}${path}`;
+    console.log(url)
+    const response = await axios.get(url, {
+      headers: {
+        'Authorization': process.env.TAOSTATS_API_KEY
+      }
+    });
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error('Error forwarding request:', error.message);
+
+    if (error.response) {
+      res.status(error.response.status).json({
+        error: error.response.data || 'Error from upstream API'
+      });
+    } else {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
